9fcc0a940939f8e19693bd24dbab0492e63e344d
zameernagaral <zameercodes@gmail.com>
2025-10-07 22:34:20 +0530

    removed error

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const methodOverride = require('method-override');
 const Listing = require('./models/listing');
 const Mongo_URL = 'mongodb://127.0.0.1:27017/stayeasy';
 const ejsMate = require('ejs-mate');
+const wrapAsync = require('./utils/wrapAsync.js');
+const ExpressError = require('./utils/ExpressError.js');
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -13,9 +16,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.engine('ejs', ejsMate);
 
+
+
 main()
-.then(() => console.log('Database connected'))
-.catch(err => console.log(err));
+    .then(() => console.log('Database connected'))
+    .catch(err => console.log(err));
 
 async function main() {
     await mongoose.connect(Mongo_URL);
@@ -35,30 +40,40 @@ app.get('/listings', (req, res) => {
 app.get('/listings/new', (req, res) => {
     res.render('listings/new');
 })
-app.post('/listings', async(req, res) => {
+app.post('/listings', wrapAsync(async (req, res , next) => {
     const listing = await Listing.create(req.body);
     res.redirect(`/listings`);
-})
-app.delete('/listings/:id', async(req, res) => {
-    let {id} = req.params;
+}))
+app.delete('/listings/:id', wrapAsync(async (req, res, next) => {
+    let { id } = req.params;
     await Listing.findByIdAndDelete(id);
     res.redirect('/listings');
-})
-app.get('/listings/:id/edit', async(req, res) => {
-    let {id} = req.params;
+}))
+app.get('/listings/:id/edit', wrapAsync(async (req, res, next) => {
+    let { id } = req.params;
     const listing = await Listing.findById(id);
     res.render('listings/edit', { listing });
-})
-app.put('/listings/:id', async(req, res) => {
-    let {id} = req.params;
-    const listing = await Listing.findByIdAndUpdate(id, req.body, {runValidators: true, new: true});
+}))
+app.put('/listings/:id', wrapAsync(async (req, res, next) => {
+    let { id } = req.params;
+    const listing = await Listing.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
     res.redirect(`/listings/${listing._id}`);
-})
-app.get('/listings/:id', async(req, res) => {
-    let {id} = req.params;
+}))
+app.get('/listings/:id', wrapAsync(async (req, res, next) => {
+    let { id } = req.params;
     const listing = await Listing.findById(id);
     res.render('listings/show', { listing });
-})
+}))
+
+app.all("*", (req, res, next) => {
+    next(new ExpressError(404, "Page not found"));
+});
+
+
+app.use((err, req, res, next) => {
+    const { status = 500, message = 'Something went wrong' } = err;
+    res.status(status).send(message);
+});
 app.listen(8080, () => {
     console.log('Server is running on port 8080');
 });
